dbc2ce2d736edd886730d6f91f2b649faf48d3f0
jeevitha <jeevitharamesh0123.com>
2026-02-24 14:13:38 +0100

    Diet Application

---
+++ my-app/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+//HOME PAGE - Intro Screen
+
+import React from "react";
+
+const Home = () => {
+  return (
+  <div className="home-container">
+  <h1 className="logo-text">DIETIFY</h1>
+  <p className="subtitle">Eat Better. Live Better.</p>
+</div>
+  );
+};
+
+export default Home;
