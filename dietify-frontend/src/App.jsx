6e2d380218690a88b1f7276001ae03616525016d
jeevitha <jeevitharamesh0123.com>
2026-03-08 17:27:51 +0100

    feat: set up application entry and routing

--- dietify-frontend/src/App.jsx
+++ dietify-frontend/src/App.jsx
@@ -7,26 +7,20 @@ import Registration from "./pages/Registration";
 import UserInput from "./pages/UserInput";
 import Result from "./pages/Result";
 import Cart from "./pages/Cart";
-import Privacy from "./pages/Privacy";
-import Terms from "./pages/Terms";
-
+import Legal from "./pages/Legal";
 
 import ProtectedRoute from "./components/common/ProtectedRoute";
 
 export default function App() {
   return (
     <Routes>
-     
       <Route path="/" element={<Logo />} />
 
-    
       <Route path="/home" element={<Home />} />
       <Route path="/about" element={<AboutUs />} />
       <Route path="/registration" element={<Registration />} />
-      <Route path="/privacy" element={<Privacy />} />
-      <Route path="/terms" element={<Terms />} />
+      <Route path="/legal" element={<Legal />} />
 
-  /
       <Route
         path="/user-input"
         element={
@@ -35,7 +29,7 @@ export default function App() {
           </ProtectedRoute>
         }
       />
-{/* Protected routes - Only accessible if "logged in" (demo auth) */}
+
       <Route
         path="/result"
         element={
@@ -54,7 +48,6 @@ export default function App() {
         }
       />
 
-     
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
